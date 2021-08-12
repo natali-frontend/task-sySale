4088c50a675ae93004d95d43999f9ef89f79981f
Nataliya Rohochaya <lc4821577@gmail.com>
2021-08-12 14:00:38 +0300

    initial commit

---
+++ src/components/App/App.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+// assets
+import styles from './App.module.scss';
+
+function App() {
+  return (
+    <div className={styles.app}>
+      1
+    </div>
+  );
+}
+
+export default App;
