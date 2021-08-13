76817a91005f03dfa307ebd2467c6c7fe0b48a11
Nataliya Rohochaya <lc4821577@gmail.com>
2021-08-13 10:30:38 +0300

    feature: add layout

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,14 +1,53 @@
-import React from "react";
+import React from 'react';
 
 // assets
 import styles from './App.module.scss';
+import productImage1_1 from '../../images/product-image-1.1.jpg'
+import productImage1_2 from '../../images/product-image-1.2.jpg'
+import productImage2_1 from '../../images/product-image-2.1.jpg'
+import productImage2_2 from '../../images/product-image-2.2.jpg'
+import productImage3_1 from '../../images/product-image-3.1.jpg'
+import productImage3_2 from '../../images/product-image-3.2.jpg'
+import { ProductItem } from "../ProductsItem/ProductsItem";
+
+const items = [
+    {
+        image: productImage1_1,
+        image2: productImage1_2,
+        title: "Шампунь",
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco.",
+        price: 200
+    },
+    {
+        image: productImage2_1,
+        image2: productImage2_2,
+        title: "Шампунь",
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco.",
+        price: 300
+    },
+    {
+        image: productImage3_1,
+        image2: productImage3_2,
+        title: "Шампунь",
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco.",
+        price: 350
+    }
+];
 
 function App() {
-  return (
-    <div className={styles.app}>
-      1
-    </div>
-  );
+    return (
+        <main>
+            <section className={styles.products}>
+                <div className='container'>
+                    <div className={styles.productsList}>
+                        {items.map((item, index) => (
+                            <ProductItem key={index} item={item} />
+                        ))}
+                    </div>
+                </div>
+            </section>
+        </main>
+    );
 }
 
 export default App;
