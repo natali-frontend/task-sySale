38b0ea553d43c794c08575d389710c0146a23fef
Nataliya Rohochaya <lc4821577@gmail.com>
2021-08-13 13:02:07 +0300

    fix: styles input radio

--- src/components/ProductFilter/ProductFilter.jsx
+++ src/components/ProductFilter/ProductFilter.jsx
@@ -24,18 +24,18 @@ export const ProductFilter = ({item, count, setCount, filterData = ['Желты�
                 <span className={styles.price}>{item.price} грн</span>
             </div>
             <form>
-                <label>
-                    <input type='radio' name='volume' />
-                    100 мл
-                </label>
-                <label>
-                    <input type='radio' name='volume' />
-                    200 мл
-                </label>
-                <label>
-                    <input type='radio' name='volume' />
-                    300 мл
-                </label>
+                <div className={styles.radio}>
+                    <input type='radio' name='radio' id='radio1' />
+                    <label htmlFor='radio1'>100 мл</label>
+                </div>
+                <div className={styles.radio}>
+                    <input type='radio' name='radio' id='radio2' />
+                    <label htmlFor='radio2'>200 мл</label>
+                </div>
+                <div className={styles.radio}>
+                    <input type='radio' name='radio' id='radio3' />
+                    <label htmlFor='radio3'>300 мл</label>
+                </div>
             </form>
             <div className={styles.filterBottom}>
                 <div className={styles.quantity}>
@@ -44,7 +44,7 @@ export const ProductFilter = ({item, count, setCount, filterData = ['Желты�
                                 onClick={() => setCount((prev) => Math.max(prev - 1, 1))}>-
                         </button>
                         <label>
-                            <input type='text' value={count} />
+                            <input type='text' value={count} readOnly />
                         </label>
                         <button type='button' className='enlarge'
                                 onClick={() => setCount((prev) => Math.min(prev + 1, 10))}>+
